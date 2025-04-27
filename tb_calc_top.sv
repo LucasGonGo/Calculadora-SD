263dac4ee47f38c23f3d7f9d525f182e7bd7ddf9
Lucas G <lucasgomes062005@gmail.com>
2025-04-27 10:46:10 -0300

    tb novo, testa todas operações e backspace, talvez eu coloque um teste pra dar erro e ver oq dá

--- tb_calc_top.sv
+++ tb_calc_top.sv
@@ -33,29 +33,52 @@ module tb_calc_top;
     reset = 1; #2;
     reset = 0; 
 
-  // Teste 1
+  // Teste 1: 123 + 1 =
     cmd = 4'd1; #20; // 1
     cmd = 4'd2; #20; // 2
     cmd = 4'd3; #20; // 3
     cmd = 4'b1010; #30; // +
-    cmd = 4'd1; #10; // 1
+    cmd = 4'd1; #20; // 1
     cmd = 4'b1110; #60; // 124
-    $finish;
     
+    #20;
+    reset = 1; #2;
+    reset = 0;
 
-  // Teste 2
-  //  cmd = 4'd5; #20;
-  //  cmd = 4'd0; #20;
-  //  cmd = 4'b1011; #20;
-  //  cmd = 4'd1; #20;
-  //  cmd = 4'd5; #20;
-  //  cmd = 4'b1110; #20;
 
-  // Teste 3
-  // cmd = 4'd6; #20;
-  // cmd = 4'b1100; #20;
-  // cmd = 4'd2; #20;
-  // cmd = 4'b1110; #20;
+  // Teste 2: 50 - 15 =
+    cmd = 4'd5; #20;
+    cmd = 4'd0; #20;
+    cmd = 4'b1011; #20;
+    cmd = 4'd1; #20;
+    cmd = 4'd5; #20;
+    cmd = 4'b1110; #20;
+
+    #20;
+    reset = 1; #2;
+    reset = 0; 
+
+
+  // Teste 3: 6 * 2 =
+   cmd = 4'd6; #20;
+   cmd = 4'b1100; #20;
+   cmd = 4'd2; #20;
+   cmd = 4'b1110; #100; // demora mais pra fazer a multiplicação
+
+    #20;
+    reset = 1; #2;
+    reset = 0; 
+
+
+    // Teste 4: 456 -> backspace -> 45
+    cmd = 4'd4; #20;
+    cmd = 4'd5; #20;
+    cmd = 4'd6; #20;
+    cmd = 4'b1111; #30;
+    cmd = 4'b1110; #60;  
+
+    #20;
+    $finish;
 
   end
 
